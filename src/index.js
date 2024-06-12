f7b8cb2eb757f80ac9eafbb6b90a6cf9faf264d9
matias <matiasfrancesco04@gmail.com>
2024-06-12 17:10:57 +0200

    :hammer: add router and UI library

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Button, ChakraProvider} from "@chakra-ui/react";
+
+const router = createBrowserRouter([
+  {
+    path: '*',
+    element: <div>404</div>,
+  },
+  {
+    path: "/",
+    element: <div>HOME PAGE</div>,
+  },
+  {
+    path: "/about",
+    element: <div>ABOUT</div>,
+  },
+]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ChakraProvider>
+      <div>
+        <RouterProvider router={router}/>
+      </div>
+    </ChakraProvider>
   </React.StrictMode>
 );
