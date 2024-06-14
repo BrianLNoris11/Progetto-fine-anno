8628c6886d77d816b016879f88485dd5b00a2018
matias <matiasfrancesco04@gmail.com>
2024-06-15 00:09:56 +0200

    :lipstick: add a better and responsive navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,39 +1,162 @@
-import {Button} from "@chakra-ui/react";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Text,
+  useMediaQuery,
+} from "@chakra-ui/react";
 // @ts-ignore
-import styles from "./navbar.module.css"
-import {useLocation, useNavigate,} from "react-router-dom";
-
+import styles from "./navbar.module.css";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const Navbar: React.FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [isLargerThen800] = useMediaQuery("(min-width: 800px)");
 
-        const navigate = useNavigate();
-        const location = useLocation();
-
-        return <div className={styles.navbar}>
+  return (
+    <header className={styles.navbar}>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          fontSize="2xl"
+          fontWeight="bold"
+          color="white"
+          fontStyle="monospace"
+          style={{ width: "60%" }}
+        >
+          F45 Training Brussels Central Station
+        </Text>
+        {isLargerThen800 ? (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              gap: "2em",
+            }}
+          >
             <Button
-                colorScheme={location.pathname === "/" && "orange"}
-                textColor={location.pathname === "/" ? "black" : "white"}
-                onClick={()=> {
-                    navigate("/")
-                }}
-                className={styles.btn}
-            >Home</Button>
+              colorScheme={location.pathname === "/" && "orange"}
+              textColor={location.pathname === "/" ? "black" : "white"}
+              onClick={() => {
+                setOpenDrawer(false);
+                navigate("/");
+              }}
+              className={styles.MenuBtn}
+            >
+              Home
+            </Button>
             <Button
-                colorScheme={location.pathname === "/about" && "orange"}
-                textColor={location.pathname === "/about" ? "black" : "white"}
-                onClick={()=> {
-                    navigate("/about")
-                }}
-                className={styles.btn}
-            >About</Button>
+              colorScheme={location.pathname === "/about" && "orange"}
+              textColor={location.pathname === "/about" ? "black" : "white"}
+              onClick={() => {
+                setOpenDrawer(false);
+                navigate("/about");
+              }}
+              className={styles.MenuBtn}
+            >
+              About
+            </Button>
             <Button
-                colorScheme={location.pathname === "/pippo" && "orange"}
-                textColor={location.pathname === "/pippo" ? "black" : "white"}
-                onClick={()=> {
-                    navigate("/pippo")
-                }}
-                className={styles.btn}
-            >Pippo</Button>
-        </div>
-
-}
+              colorScheme={location.pathname === "/pippo" && "orange"}
+              textColor={location.pathname === "/pippo" ? "black" : "white"}
+              onClick={() => {
+                setOpenDrawer(false);
+                navigate("/pippo");
+              }}
+              className={styles.MenuBtn}
+            >
+              Pippo
+            </Button>
+          </div>
+        ) : (
+          <>
+            <Button
+              textColor="black"
+              onClick={() => {
+                setOpenDrawer(true);
+              }}
+              colorScheme="orange"
+              style={{ width: "auto" }}
+            >
+              OPEN
+            </Button>
+            <Drawer
+              isOpen={openDrawer}
+              onClose={() => {
+                setOpenDrawer(false);
+              }}
+            >
+              <DrawerOverlay />
+              <DrawerContent className={styles.drawer}>
+                <DrawerCloseButton color="white" />
+                <DrawerHeader color="white" style={{ marginBottom: "1em" }}>
+                  Menu
+                </DrawerHeader>
+                <DrawerBody
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    gap: "2em",
+                  }}
+                >
+                  <Button
+                    colorScheme={location.pathname === "/" && "orange"}
+                    textColor={location.pathname === "/" ? "black" : "white"}
+                    onClick={() => {
+                      setOpenDrawer(false);
+                      navigate("/");
+                    }}
+                    className={styles.MenuBtn}
+                  >
+                    Home
+                  </Button>
+                  <Button
+                    colorScheme={location.pathname === "/about" && "orange"}
+                    textColor={
+                      location.pathname === "/about" ? "black" : "white"
+                    }
+                    onClick={() => {
+                      setOpenDrawer(false);
+                      navigate("/about");
+                    }}
+                    className={styles.MenuBtn}
+                  >
+                    About
+                  </Button>
+                  <Button
+                    colorScheme={location.pathname === "/pippo" && "orange"}
+                    textColor={
+                      location.pathname === "/pippo" ? "black" : "white"
+                    }
+                    onClick={() => {
+                      setOpenDrawer(false);
+                      navigate("/pippo");
+                    }}
+                    className={styles.MenuBtn}
+                  >
+                    Pippo
+                  </Button>
+                </DrawerBody>
+              </DrawerContent>
+            </Drawer>
+          </>
+        )}
+      </div>
+    </header>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider} from "@chakra-ui/react";
-import {Navbar} from "./components/navbar/navbar.tsx";
 import {Router} from "./router.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
